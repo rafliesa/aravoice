e83e01341e27b073d5797689eed01166ad8aaa33
rafliesa <rflisaa@gmail.com>
2026-06-14 15:59:09 +0700

    Add Para Pop rubrik page

---
+++ app/para-pop/page.tsx
@@ -0,0 +1,10 @@
+import RubrikPage from "@/components/rubrik/RubrikPage";
+
+export default function ParaPopPage() {
+  return (
+    <RubrikPage
+      category="Para Pop"
+      description="Cerita budaya populer, komunitas, gaya hidup, dan ruang kreatif yang tumbuh bersama gerakan inklusi."
+    />
+  );
+}
