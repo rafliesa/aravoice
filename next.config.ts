df5541ff4c39fe910ab4df02434a4063cdd7c0d8
rafliesa <rflisaa@gmail.com>
2026-06-23 03:29:00 +0700

    feat: add rewrites for uploads API and remove public uploads directory

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,14 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/uploads/:path*",
+        destination: "/api/uploads/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
