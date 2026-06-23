7473a127f405accabf2987535982e890b5573505
rafliesa <rflisaa@gmail.com>
2026-06-23 16:56:04 +0700

    feat: enable dynamic rendering for AboutPage component

--- app/tentang-kami/page.tsx
+++ app/tentang-kami/page.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import AboutPage from "@/modules/about";
 
+export const dynamic = "force-dynamic";
+
 export const metadata: Metadata = {
   title: "Tentang Kami",
   description:
